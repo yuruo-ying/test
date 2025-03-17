2b333b489f3503845a53400ebf28a9ba95bb2feb
yuruo-ying <69719283+yuruo-ying@users.noreply.github.com>
2025-03-17 15:06:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,9 @@
 from PIL import ImageGrab, Image
 import time
 
-im1 = ImageGrab.grab() 
-# im1 = ImageGrab.grab(300,100,1400,600)
-TimeName = time.strftime("%Y%m%d%H%M%S", time.localtime())
-path = '.\\'+str(TimeName)+'.jpg'
-im1.save(path)
-im1.show()  # 展示
 
 
-im2 = ImageGrab.grabclipboard() 
-# 判断粘贴板是否为空
-if isinstance(im2, Image.Image):
-    im2.save('.\\a.jpg')
-else:
-    print("no no no ")
+m1 = ImageGrab.grabclipboard()
+if st.button("save"):
+    m1.save('test_image.png')
 
