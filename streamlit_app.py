ef7032727b316c8cb62a28b337a0bc84af496cde
yuruo-ying <69719283+yuruo-ying@users.noreply.github.com>
2025-03-17 14:48:23 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 123 new app")
+st.title("🎈 1234 new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
